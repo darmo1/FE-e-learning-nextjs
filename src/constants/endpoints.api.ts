ff7f722bca4f4d1dafa2838fb96b607bcc0f2ebd
Danilo Morales <darmito10@gmail.com>
2025-04-28 18:47:51 -0500

    feature(dev):0002 Added some request-handler refresh token and other things

--- src/constants/endpoints.api.ts
+++ src/constants/endpoints.api.ts
@@ -1,8 +1,7 @@
-const HOST = process.env.HOST_FRONTEND || "http://localhost:3000";
-const NEXT_HOST_FRONTEND= process.env.NEXT_PUBLIC_HOST_FRONTEND 
+const HOST = process.env.HOST_FRONTEND || process.env.NEXT_PUBLIC_HOST_FRONTEND || "https://goproclass.vercel.app";
 
 export const apiEndpoints = {
-  UPLOAD_VIDEO: `${NEXT_HOST_FRONTEND}/api/upload-video`,
+  UPLOAD_VIDEO: `${HOST}/api/upload-video`,
   UPLOAD_IMAGE: `${HOST}/api/upload-image`,
   COOKIE: `${HOST}/api/cookie`,
 };
