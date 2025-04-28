419c3c80d449bf9a53c4b5b83c000e3274546ecd
Danilo Morales <darmito10@gmail.com>
2025-04-28 18:32:31 -0500

    feature(dev):0002 Added some request-handler refresh token and other things

--- src/constants/endpoints.api.ts
+++ src/constants/endpoints.api.ts
@@ -2,7 +2,7 @@ const HOST = process.env.HOST_FRONTEND || "http://localhost:3000";
 const NEXT_HOST_FRONTEND= process.env.NEXT_HOST_FRONTEND 
 
 export const apiEndpoints = {
-  UPLOAD_VIDEO: `${HOST}/api/upload-video`,
-  UPLOAD_IMAGE: `${NEXT_HOST_FRONTEND}/api/upload-image`,
+  UPLOAD_VIDEO: `${NEXT_HOST_FRONTEND}/api/upload-video`,
+  UPLOAD_IMAGE: `${HOST}/api/upload-image`,
   COOKIE: `${HOST}/api/cookie`,
 };
