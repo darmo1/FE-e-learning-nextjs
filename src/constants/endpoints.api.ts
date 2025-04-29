48a9df09f2f84aa5edbe08b56193eae4e0cefdcf
Danilo Morales <darmito10@gmail.com>
2025-04-28 23:50:12 -0500

    feature(dev):0002 Added getapiurl function

--- src/constants/endpoints.api.ts
+++ src/constants/endpoints.api.ts
@@ -5,3 +5,9 @@ export const apiEndpoints = {
   UPLOAD_IMAGE: `${HOST}/api/upload-image`,
   COOKIE: `${HOST}/api/cookie`,
 };
+
+
+export function getApiUrl(path: string) {
+  const baseUrl = process.env.NEXT_PUBLIC_HOST_FRONTEND || "http://localhost:3000";
+  return `${baseUrl}${path.startsWith("/") ? path : `/${path}`}`;
+}
